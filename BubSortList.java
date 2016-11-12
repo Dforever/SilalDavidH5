f31d026d596fa7f804eac16a796cc615503560c1
David Silal <dsilal@albany.edu>
2016-11-12 13:47:54 -0500

    Beginning BubbleSortedList

---
+++ BubSortList.java
@@ -0,0 +1,10 @@
+public class BubSortList {
+	//Max size of ArrayList will equal the size of Array
+	public final int MAXSIZE = 10;
+
+	//This will be the array for the list
+	private int[] ray;
+
+	//This will be the length of list
+	private int nowLength;
+
