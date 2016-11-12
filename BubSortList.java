97e43275a3b18269d3749120493ccde36e57ed1a
David Silal <dsilal@albany.edu>
2016-11-12 13:51:37 -0500

    The method utilizing the sorting added

--- BubSortList.java
+++ BubSortList.java
@@ -21,3 +21,17 @@ public class BubSortList {
 				nowLength++;
 			}
 	}
+
+	//The method for sorting thats being used
+		public void sort() {
+			for (int j = nowLength - 1; j > 1; j--) {
+				for (int i = 0; i < j; i++) {
+					if (ray[i] > ray[i+1]) {
+						int tmp = ray [i];
+						ray[i] = ray[i+1];
+						ray[i+1] = tmp;
+					}
+				}
+				print();
+			}
+	}
