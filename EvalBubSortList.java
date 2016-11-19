6f84ef6c84390389d494c0195e016a0a577b9e0b
David Silal <dsilal@albany.edu>
2016-11-19 14:06:59 -0500

    Creates any number between 20-100, then will add item to list

--- EvalBubSortList.java
+++ EvalBubSortList.java
@@ -9,4 +9,10 @@ public class EvalBubSortList {
 		BubSortList listSorted = new BubSortList();
 		//This will make the list full of Random #'s
 		for (int i = 0; i < listSorted.MAXSIZE; i++)
+		{
+					//This will create any number that is 20-100
+					int numRandom = rnd.nextInt(100 - 20 + 1) + 20;
+					//This will add item to the list
+					listSorted.add(numRandom);
+		}
 
