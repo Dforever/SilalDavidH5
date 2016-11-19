97c41d26a44b1936dce6ffab054c085714c618a5
David Silal <dsilal@albany.edu>
2016-11-19 14:00:07 -0500

    Beginning of Random NUmber Generator

---
+++ EvalBubSortList.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+
+public class EvalBubSortList {
+	public static void main(String arg[]) {
+		//Will start to begin the random number generatror and load it
+		Random rnd = new Random();
+
